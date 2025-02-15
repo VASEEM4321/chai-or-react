fd31ee7dc2baf0fcfe23d9e809effb3bc8a13149
Md Vaseem <mohdvaseem025771@gmail.com>
2025-02-15 19:36:02 +0530

    counter by using useState react hook, completed

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -4,30 +4,28 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ //let counter = 10;
+ 
+ let [counter, setCounter] = useState(10)
+
+ const decreaseValuse = () => {
+  setCounter(counter - 1);
+ }
+
+ const increase = () => {
+  setCounter(counter + 1);
+ }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>chai aur react</h1>
+     <h2>counter value: {counter}</h2>
+     <button
+     onClick={increase}
+     >increase value</button>
+     <button
+     onClick={decreaseValuse} 
+     >Decrease value</button>
     </>
   )
 }
