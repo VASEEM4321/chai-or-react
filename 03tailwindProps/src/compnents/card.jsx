f32bfb3545826601276fbd471b877ef295911a57
Md Vaseem <mohdvaseem025771@gmail.com>
2025-02-17 20:32:40 +0530

    03tailwindProps done

---
+++ 03tailwindProps/src/compnents/card.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Card({username, name}) {
+  return (
+    <figure class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+  <img class="w-24 h-24 rounded-full mx-auto" src="https://images.unsplash.com/photo-1739382122846-74e722a6eea4?w=300&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw4Mnx8fGVufDB8fHx8fA%3D%3D" alt="" width="384" height="512" />
+  <div class="pt-6 text-center space-y-4">
+    <blockquote>
+      <p class="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption class="font-medium">
+      <div class="text-sky-500 dark:text-sky-400">
+        {name}
+      </div>
+      <div class="text-sky-500 dark:text-slate-500">
+        {username}
+      </div>
+    </figcaption>
+  </div>
+</figure>
+  )
+}
+
+export default Card
